85ec1fda837b9654ebf37045f2f8d2c272f2d7d1
D.S. <85100629+seclusioner@users.noreply.github.com>
2025-06-13 17:01:34 +0800

    Update main.py
    Add custom parameters

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def make_args_parser():
 
     ##### Dataset #####
     parser.add_argument(
-        "--dataset_name", required=True, type=str, choices=["scannet", "sunrgbd"]
+        "--dataset_name", required=True, type=str, choices=["scannet", "sunrgbd", "custom"]
     )
     parser.add_argument(
         "--dataset_root_dir",
